fbe322917abce82a432be6a40880fa61d95ab590
Nicolas "P.B." ROLLE <nicolas_rolle@openfreeway.fr>
2020-03-30 20:42:51 +0200

    Append comment and constructor.

--- src/main/java/com/kawa/api/KawaSystemApiApplication.java
+++ src/main/java/com/kawa/api/KawaSystemApiApplication.java
@@ -6,17 +6,41 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Application.
+ *
+ * @author <a href="https://github.com/fduquesne">Florian DUQUESNE</a>
+ * @version 0.1.0 hydrogen
+ */
 @SpringBootApplication
 @RestController
 public class KawaSystemApiApplication {
 
-    public static void main(String[] args) {
+    /**
+     * Constructor.
+     * @author Nicolas ROLLE
+     * @since 0.1.0 hydrogen
+     */
+    public KawaSystemApiApplication() {
+        super();
+    }
+
+    /**
+     * Main {@code method}.
+     * @param args Native Arguments.
+     */
+    public static void main(final String[] args) {
         SpringApplication.run(KawaSystemApiApplication.class, args);
     }
 
+    /**
+     * Hello World Service.
+     * @param name The name to display.
+     * @return The response.
+     */
     @GetMapping("/hello")
-    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return String.format("Hello %s!", name);
+    public static String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name); //$NON-NLS-1$
     }
 
 }
