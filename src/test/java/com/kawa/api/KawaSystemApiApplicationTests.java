50d7f8e440d48e3de25a1d287f6864977dd90d16
fduquesne <fduquesne@icloud.com>
2020-03-30 15:58:52 +0200

    build : initialize spring.io project

---
+++ src/test/java/com/kawa/api/KawaSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kawa.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KawaSystemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
