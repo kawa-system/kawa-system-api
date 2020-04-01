d01ade2efef5c2be22fe8e25013271dbc106b1de
old-papa-bear <>
2020-04-01 18:50:59 +0200

    Append global constants class

---
+++ src/main/java/com/kawa/api/constants/Constants.java
@@ -0,0 +1,31 @@
+package com.kawa.api.constants;
+
+/**
+ * <b>Contants</b> <code>class</code>.
+ * <br>
+ *
+ * The purpose of this <code>class</code> is to provide global constant
+ * to the project.
+ * <hr>
+ *
+ * @since 0.1.0 hydrogen
+ * @author Nicolas "Papa Bear" ROLLE
+ */
+public final class Constants {
+
+    /** Global Serial Unique Version ID. */
+    public static final long SUID = 0x01_00_0001_00000000L;
+
+    /**
+     * Constructor.
+     * <i>As the purpose of this <code>class</code> is to provide static fields
+     * only, there's no reason to create new instance of this one. So, the
+     * constructor access will be restrict to <code>private</code>.</i>
+     * <hr>
+     * @since 0.1.0 hydrogen
+     */
+    private Constants() {
+        super();
+    }
+
+}
---
+++ src/main/java/com/kawa/api/constants/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * <b>Constants</b> <code>package</code>.
+ * <br>
+ *
+ * The purpose of this package is to provide class(es) of constants.
+ * <hr>
+ *
+ * @since 0.1.0 hydrogen
+ */
+package com.kawa.api.constants;
