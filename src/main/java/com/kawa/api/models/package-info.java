496b805efe40b792bf4629f0dce712dcfbce1b44
Nicolas "Papa Bear" ROLLE <>
2020-04-01 15:17:52 +0200

    append package info files

---
+++ src/main/java/com/kawa/api/models/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * <b>Models</b> <code>package</code>.
+ * <br>
+ *
+ * The purpose of this package is to provide the classes used to represent
+ * the project data model.
+ * <hr>
+ *
+ * @since 0.1.0 hydrogen
+ */
+package com.kawa.api.models;
