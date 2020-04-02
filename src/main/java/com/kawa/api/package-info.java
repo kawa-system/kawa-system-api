3e9152bf8c9a1a8d3296eb50a0900834696fe65d
old-papa-bear <>
2020-04-02 16:58:46 +0200

    Fix checkstyle issue.

---
+++ src/main/java/com/kawa/api/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * <b>ROot</b> <code>package</code>.
+ * <br>
+ *
+ * The purpose of this package is to contain every classes of this project
+ * (except test classes).
+ * project.
+ * <hr>
+ *
+ * @since 0.1.0 hydrogen
+ */
+package com.kawa.api;
